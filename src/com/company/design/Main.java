53a006953160c890ab29c70a56ad329042a9eeab
Hayden-Doo <heton.doo@gmail.com>
2023-01-29 21:25:54 +0900

    add strategy-pattern

--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -15,6 +15,7 @@ import com.company.design.proxy.IBrowser;
 import com.company.design.singleton.AClazz;
 import com.company.design.singleton.BClazz;
 import com.company.design.singleton.SocketClient;
+import com.company.design.strategy.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -129,11 +130,34 @@ public class Main {
 //        ftpClient.disConnect();
 
 //        With Facade
-        SftpClient sftpClient = new SftpClient("www.doo.com", 22, "/home", "text.txt");
-        sftpClient.connect();
-        sftpClient.write();
-        sftpClient.read();
-        sftpClient.disConnect();
+//        SftpClient sftpClient = new SftpClient("www.doo.com", 22, "/home", "text.txt");
+//        sftpClient.connect();
+//        sftpClient.write();
+//        sftpClient.read();
+//        sftpClient.disConnect();
+
+//        strategy
+        Encoder encoder = new Encoder();
+
+        EncodingStrategy base64 = new Base64Strategy();
+
+        EncodingStrategy normal = new NomalStrategy();
+
+        String message = "hello java";
+
+        encoder.setEncodingStrategy(base64);
+        String base64Result = encoder.getMessage(message);
+        System.out.println(base64Result);
+
+        encoder.setEncodingStrategy(normal);
+        String normalResult = encoder.getMessage(message);
+        System.out.println(normalResult);
+
+        encoder.setEncodingStrategy(new AppendStrategy());
+        String appendResult = encoder.getMessage(message);
+        System.out.println(appendResult);
+
+
     }
 
     // 콘센트
